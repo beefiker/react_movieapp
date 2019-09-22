07420ebb9a1f7ca444911eed7900aa8babfd05ec
beefikerm <fiker@naver.com>
2019-09-23 01:13:25 +0900

    this is my first movieapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
-  state = {
+  /* state = {
     count: 0
   };
   add = () => {
     this.setState({ count: this.state.count + 1 }); // state에 직접적으로 기대는 효율이 나쁜 방법
   };
   minus = () => {
-    this.setState(current => ({ count: current.count - 1 })); // 효율이 좋은 방법
-  };
-  render() {
-    return (
-      <div>
-        <h1> The number is {this.state.count}</h1>
+    this.setState(current => ({ count: current.count - 1 })); // 현재 state를 얻어서 효율이 좋은 방법
+  }; 
+  <h1> The number is {this.state.count}</h1>
         <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
-      </div>
+        */
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    //async와 await을 사용해 접근이 끝날때까지기다리라고 명령
+    const {
+      data: {
+        data: { movies }
+      } // 데이터\데이터\무비 경로 표현은 최신 es6 자바스크립트의 data:{data:{movies}}로 표현 가능
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text"> Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
