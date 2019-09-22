d43e69d02e94b257c76596cffc86a7b53f4ed3bc
beefikerm <fiker@naver.com>
2019-09-23 01:14:42 +0900

    Updates

---
+++ precache-manifest.98723806354f646d9d92787371910480.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50171fe0170813bc9c9a016ad0308975",
+    "url": "/react_movieapp/index.html"
+  },
+  {
+    "revision": "0529525fac0e333f0542",
+    "url": "/react_movieapp/static/css/main.6262bd94.chunk.css"
+  },
+  {
+    "revision": "4aff7a1365111b29ab51",
+    "url": "/react_movieapp/static/js/2.d5e1489f.chunk.js"
+  },
+  {
+    "revision": "0529525fac0e333f0542",
+    "url": "/react_movieapp/static/js/main.7f77c81d.chunk.js"
+  },
+  {
+    "revision": "be10a72e1faa81b937b5",
+    "url": "/react_movieapp/static/js/runtime~main.dcb94441.js"
+  }
+]);
